7ad868d6604814f8c433e7d4e66365b6321d570b
enespolat24 <enespolat24@gmail.com>
2024-02-20 23:02:55 +0300

    create userController

--- src/main/java/org/enesp/authservice/controller/UserController.java
+++ src/main/java/org/enesp/authservice/controller/UserController.java
@@ -1,2 +1,28 @@
-package org.enesp.authservice.controller;public class UserController {
+package org.enesp.authservice.controller;
+
+import org.enesp.authservice.model.AuthUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.enesp.authservice.repository.UserRepository;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users")
+    public List<AuthUser> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/token")
+    public String getToken() {
+        return "token";
+    }
 }

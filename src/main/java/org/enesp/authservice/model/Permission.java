ae02933788b25d9878ec959261f964882e81f9b2
enespolat24 <enespolat24@gmail.com>
2024-02-20 23:02:20 +0300

    create permission and role models

--- src/main/java/org/enesp/authservice/model/Permission.java
+++ src/main/java/org/enesp/authservice/model/Permission.java
@@ -1,2 +1,17 @@
-package org.enesp.authservice.model;public class Permission {
+package org.enesp.authservice.model;
+
+import jakarta.persistence.*;
+
+import java.util.Collection;
+
+@Entity
+public class Permission {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "permissions")
+    private Collection<Role> roles;
 }
--- src/main/java/org/enesp/authservice/model/Role.java
+++ src/main/java/org/enesp/authservice/model/Role.java
@@ -1,2 +1,23 @@
-package org.enesp.authservice.model;public class Role {
+package org.enesp.authservice.model;
+
+import jakarta.persistence.*;
+
+import java.util.Collection;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    @ManyToMany(mappedBy = "roles")
+    private Collection<AuthUser> authUsers;
+
+    @ManyToMany()
+    @JoinTable(
+            name = "role_permission",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id")
+    )
+    private Collection<Permission> permissions;
 }

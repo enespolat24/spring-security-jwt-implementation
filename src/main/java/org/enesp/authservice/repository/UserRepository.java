6dc25d94ee87a9d2d17235d9283b52f72ac65c5d
enespolat24 <enespolat24@gmail.com>
2024-02-21 00:08:50 +0300

    add two methods in user repo

--- src/main/java/org/enesp/authservice/repository/UserRepository.java
+++ src/main/java/org/enesp/authservice/repository/UserRepository.java
@@ -6,4 +6,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<AuthUser, Long> {
+
+    AuthUser findByEmail(String email);
+
+    AuthUser findByUsername(String username);
+
 }

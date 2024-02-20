6453d6e63a47390f296bf18076252aa34f042a7a
enespolat24 <enespolat24@gmail.com>
2024-02-21 00:09:01 +0300

    mini refactor for the controller

--- src/main/java/org/enesp/authservice/controller/UserController.java
+++ src/main/java/org/enesp/authservice/controller/UserController.java
@@ -1,28 +1,52 @@
 package org.enesp.authservice.controller;
 
 import org.enesp.authservice.model.AuthUser;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.enesp.authservice.util.JwtUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 import org.enesp.authservice.repository.UserRepository;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/authenticate")
 public class UserController {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
-    @GetMapping("/users")
-    public List<AuthUser> getUsers() {
-        return userRepository.findAll();
+    @PostMapping("/register")
+    public String createUser(@RequestBody AuthUser user) {
+        if (userRepository.findByEmail(user.getEmail()) != null){
+            throw new RuntimeException("email already in use");
+        } else if (userRepository.findByUsername(user.getUsername()) != null) {
+            throw new RuntimeException("username already in use");
+        }
+
+        var AuthUser = new AuthUser();
+        AuthUser.setUsername(user.getUsername());
+        AuthUser.setEmail(user.getEmail());
+        AuthUser.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(AuthUser);
+        String token = JwtUtil.generateToken(AuthUser.getUsername());
+        return "{ \"message\": \"User created successfully\", \"token\": \"" + token + "\" }";
     }
 
-    @GetMapping("/token")
-    public String getToken() {
-        return "token";
+    @PostMapping("/login")
+    public String login(@RequestBody AuthUser user) {
+        var AuthUser = userRepository.findByUsername(user.getUsername());
+        if (AuthUser == null) {
+            return "username or password is invalid";
+        } else if (!passwordEncoder.matches(user.getPassword(), AuthUser.getPassword())) {
+            return "username or password is invalid";
+        }
+        return JwtUtil.generateToken(AuthUser.getUsername());
     }
 }

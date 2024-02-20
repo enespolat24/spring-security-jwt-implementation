1b7c2f90fe4cbea5e2ab0479bd1c96fff6aa935d
enespolat24 <enespolat24@gmail.com>
2024-02-21 00:07:56 +0300

    jwt util

---
+++ src/main/java/org/enesp/authservice/util/JwtUtil.java
@@ -0,0 +1,39 @@
+package org.enesp.authservice.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+
+@Service
+public class JwtUtil {
+    private static String SECRET;
+
+    private static long EXPIRATION;
+
+    @Value("${jwt.secret}")
+    public void setSecret(String secret) {
+        SECRET = secret;
+    }
+
+    @Value("${jwt.expiration}")
+    public void setExpiration(long expiration) {
+        EXPIRATION = expiration;
+    }
+
+    public static String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
+    }
+
+    public static Claims validateToken(String token) {
+        return Jwts.parser().setSigningKey(SECRET).build().parseSignedClaims(token).getPayload();
+    }
+
+}

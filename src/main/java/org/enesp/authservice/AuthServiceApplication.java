5657a35dbddb6539eceae0cfaa9624780ed3d4ee
enespolat24 <enespolat24@gmail.com>
2024-02-20 23:02:02 +0300

    security configuration

--- src/main/java/org/enesp/authservice/AuthServiceApplication.java
+++ src/main/java/org/enesp/authservice/AuthServiceApplication.java
@@ -2,12 +2,12 @@ package org.enesp.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class AuthServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
 
 }
--- src/main/java/org/enesp/authservice/config/SecurityConfig.java
+++ src/main/java/org/enesp/authservice/config/SecurityConfig.java
@@ -1,2 +1,21 @@
-package org.enesp.authservice.config;public class SecurityConfig {
+package org.enesp.authservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends AbstractSecurityWebApplicationInitializer {
+    @Bean
+    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/users/**");
+        return http.build();
+    }
 }
